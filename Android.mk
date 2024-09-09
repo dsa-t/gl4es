e82982286c0a4a16d0820fb79e6007dbe44c8137
Alex Shvartzkop <dudesuchamazing@gmail.com>
2024-09-09 13:15:32 +0300

    Build shared library with debug on Android.

--- Android.mk
+++ Android.mk
@@ -92,6 +92,7 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -O3 -fvisibility=hidden -include include/android_debug.h
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
+LOCAL_CFLAGS += -DDEBUG
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
 LOCAL_CFLAGS += -DDEFAULT_ES=2
 //TODO: maybe temporary?
@@ -100,5 +101,4 @@ LOCAL_CFLAGS += -Wno-typedef-redefinition -Wno-dangling-else
 LOCAL_LDLIBS := -llog
 #building as a static lib
 
-LOCAL_CFLAGS += -DSTATICLIB
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
