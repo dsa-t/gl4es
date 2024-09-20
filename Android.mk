b1ffd37a670fcdad691a3c17ef1f59231c69b60e
Alex Shvartzkop <dudesuchamazing@gmail.com>
2024-09-20 08:26:18 +0300

    Enable -DUSE_ANDROID_LOG

--- Android.mk
+++ Android.mk
@@ -93,6 +93,7 @@ LOCAL_CFLAGS += -g -std=gnu99 -funwind-tables -O3 -fvisibility=hidden -include i
 LOCAL_CFLAGS += -DNOX11
 LOCAL_CFLAGS += -DNO_GBM
 LOCAL_CFLAGS += -DDEBUG
+LOCAL_CFLAGS += -DUSE_ANDROID_LOG
 #LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
 LOCAL_CFLAGS += -DDEFAULT_ES=2
 //TODO: maybe temporary?
